95e5606c8f4210005ce4585e51cad75c03e526a7
Evan Klein <EKlein@misoenergy.org>
2024-12-16 08:07:48 -0500

    A little Day 12 attempt

--- problem_sets/day12.py
+++ problem_sets/day12.py
@@ -1,18 +1,26 @@
 from .problem import Problem
 
 class PlotSection():
-    def __init__(self, x_pos, y_pos, plot_value, top_neighbor, right_neighbor, bottom_neighbor, left_neighbor):
+    def __init__(self, x_pos, y_pos, plot_value):
         self.x_pos = x_pos
         self.y_pos = y_pos
         self.plot_value = plot_value
-        self.top_neighbor = top_neighbor
-        self.right_neighbor = right_neighbor
-        self.bottom_neighbor = bottom_neighbor
-        self.left_neighbor = left_neighbor
-
+    
     def __str__(self):
-        return f'Position({self.x_pos}, {self.y_pos}), Value({self.plot_value}), Top({self.top_neighbor is not None}), Right({self.top_neighbor is not None}), Bottom({self.top_neighbor is not None}), Left({self.top_neighbor is not None})'
+        return f'({self.x_pos}, {self.y_pos}, {self.plot_value})'
 
+    def __repr__(self):
+        return self.__str__()
+
+    def __eq__(self, other):
+        if isinstance(other, PlotSection):
+            return (self.x_pos == other.x_pos and
+                    self.y_pos == other.y_pos and
+                    self.plot_value == other.plot_value)
+        return False
+
+    def __hash__(self):
+        return hash((self.x_pos, self.y_pos, self.plot_value))
 class Day12(Problem):
     def __init__(self):
         self.plot_grid = []
@@ -20,14 +28,25 @@ class Day12(Problem):
     def PartA(self, input):
         grid = [[c for c in line] for line in input.splitlines()]
 
+
         self.plot_grid = [[None for _ in range(len(grid[0]))] for _ in range(len(grid))]
+        plot_set = set()
         for x, row in enumerate(grid):
             for y, cell in enumerate(row):
-                self.try_neighbor(grid, grid[x][y], x, y)
+                plot_set.add(self.get_neighbors(grid, grid[x][y], x, y))
 
-        print(self.plot_grid[0][0])
+        # sum = 0
+        # plot_groups = set()
+        # for x, row in enumerate(self.plot_grid):
+        #     for y, cell in enumerate(row):
+        #         sum += 1 if cell.top_neighbor is None else 0
+        #         sum += 1 if cell.right_neighbor is None else 0
+        #         sum += 1 if cell.bottom_neighbor is None else 0
+        #         sum += 1 if cell.left_neighbor is None else 0
+        #         # print(cell)
+        return sum
         
-    def try_neighbor(self, grid, plot_value, x, y):
+    def get_neighbors(self, grid, plot_value, x, y):
         if not (0 <= x < len(grid)) or not (0 <= y < len(grid[0])):
             return None
         
@@ -38,16 +57,14 @@ class Day12(Problem):
             return self.plot_grid[x][y]
 
         # Create a placeholder to mark this cell as being processed
-        placeholder = PlotSection(x, y, plot_value, None, None, None, None)
+        placeholder = PlotSection(x, y, plot_value)
         self.plot_grid[x][y] = placeholder
 
         # Recursively assign neighbors
-        placeholder.top_neighbor = self.try_neighbor(grid, plot_value, x + 1, y)
-        placeholder.right_neighbor = self.try_neighbor(grid, plot_value, x, y + 1)
-        placeholder.bottom_neighbor = self.try_neighbor(grid, plot_value, x - 1, y)
-        placeholder.left_neighbor = self.try_neighbor(grid, plot_value, x, y - 1)
-
-        return placeholder
+        top_neighbors = self.get_neighbors(grid, plot_value, x - 1, y)
+        right_neighbors = self.get_neighbors(grid, plot_value, x, y + 1)
+        bottom_neighbors = self.get_neighbors(grid, plot_value, x + 1, y)
+        left_neighbors = self.get_neighbors(grid, plot_value, x, y - 1)
 
     def PartB(self, input):
         pass
